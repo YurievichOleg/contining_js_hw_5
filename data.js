122050d531ab116450cf41f0a5316a402c6efc05
goodday <lyavyan@icloud.com>
2023-05-05 21:32:26 +0300

    hw_5

---
+++ data.js
@@ -0,0 +1,24 @@
+const dataInfo = `[
+    {
+        "answer": "yes",
+        "forced": false,
+        "text": "Так говорят: Да!",
+        "image": "https://yesno.wtf/assets/yes/15-3d723ea13af91839a671d4791fc53dcc.gif"
+        
+    },
+    {
+        "answer": "no",
+        "forced": false,
+        "text": "Так говорят: Нет!",
+        "image": "https://yesno.wtf/assets/no/6-4bf0a784c173f70a0cab96efd9ff80c9.gif"
+        
+    },
+    {
+        "answer": "yes",
+        "forced": false,
+        "text": "Так говорят: Да!",
+        "image": "https://yesno.wtf/assets/yes/5-64c2804cc48057b94fd0b3eaf323d92c.gif"
+        
+    }
+]
+`
